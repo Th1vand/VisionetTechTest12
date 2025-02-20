73586fa058c6a3656b7d0e91b3cc4d97ee4cd713
ferdy <ferdy@FERDY_PAVILLION>
2025-02-20 13:47:53 +0700

    Add project files.

---
+++ VisionetTechTest/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input : ");
+            string input = Console.ReadLine();
+
+            string result = Manipulate(input);
+            Console.WriteLine("Output :" + result);
+        }
+
+        static string Manipulate(string input) 
+        {
+            int midIndex = input.Length / 2;
+            
+            string str1 = input.Substring(0, midIndex);
+            string str2 = input.Substring(midIndex);
+
+            char[] chars1 = str1.ToCharArray();
+            Array.Reverse(chars1);
+            string reversed1 = new string(chars1);
+
+            char[] chars2 = str2.ToCharArray();
+            Array.Reverse(chars2);
+            string reversed2 = new string(chars2);
+
+            string result = reversed1 + reversed2;
+
+            return result;
+        }
+    }
+}
---
+++ VisionetTechTest2/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Test2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] firstWords = { "cinema", "host", "aba", "train" };
+            string[] secWords = { "iceman", "shot", "bab", "rain" };
+
+            string result =AnagramCheck(firstWords,secWords);
+            Console.WriteLine("Output :" + result);
+        }
+
+        static string AnagramCheck(string[] word1, string[] word2)
+        {
+           var ResultBuilder = new System.Text.StringBuilder();
+            
+            for (int i = 0;i < word1.Length; i++)
+            {
+                string fWord = word1[i];
+                string sWord = word2[i];
+
+                bool CheckisAnagram = isAnagram(fWord, sWord);
+
+                ResultBuilder.Append(CheckisAnagram ? "1" : "0");
+            }
+            return ResultBuilder.ToString();
+        }
+
+        static bool isAnagram(string word1, string word2)
+        {
+            if (word1.Length != word2.Length) return false;
+
+            char[] chars1 = word1.ToCharArray();
+            char[] chars2 = word2.ToCharArray();
+
+            Array.Sort(chars1);
+            Array.Sort(chars2);
+
+            return chars1.SequenceEqual(chars2);
+        }
+    }
+}
